21ab8a820f14a239297dcdd02bebf1e64ba82f3c
Bratchikov Igor <bratchikov@ics.perm.ru>
2017-09-12 20:59:29 +0500

    Fix csproj

--- NewPlatform.Flexberry.ORM.GisMSSQLDataService/Properties/AssemblyInfo.cs
+++ NewPlatform.Flexberry.ORM.GisMSSQLDataService/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NewPlatform.Flexberry.ORM.GisMSSQLDataService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("GIS Extensions for MSSQL DataService")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("New Platform Ltd")]
 [assembly: AssemblyProduct("NewPlatform.Flexberry.ORM.GisMSSQLDataService")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Flexberry PLATFORM 2017")]
+[assembly: AssemblyTrademark("Flexberry")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
